bf86026bb619a5faaaa0df6cad891aea17e8da99
ruipedromanta <39620242+ruipedromanta@users.noreply.github.com>
2018-05-29 17:16:30 +0100

    Rui Manta layouts1

--- app/src/main/java/pt/ipg/quizzprogramao/MainActivity.java
+++ app/src/main/java/pt/ipg/quizzprogramao/MainActivity.java
@@ -2,8 +2,26 @@ package pt.ipg.quizzprogramao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button resposta1, resposta2, resposta3, resposta4;
+
+    TextView score, pergunta;
+
+    private Perguntas mPerguntas = new Perguntas();
+
+    private String mResposta;
+    private int mScore;
+    private int mPerguntasLength = mPerguntas.mPerguntas.length;
+
+    Random r;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/pt/ipg/quizzprogramao/Perguntas.java
@@ -0,0 +1,23 @@
+package pt.ipg.quizzprogramao;
+
+/**
+ * Created by ruima on 29/05/2018.
+ */
+
+public class Perguntas {
+
+    public String mPerguntas [] = {
+            //Fazer conexão base de dados
+
+    };
+
+
+    private String mRespostas [] [] = {
+            // Fazer conexão base de dados
+    };
+
+
+    private String mRespostasCorretas [] = {
+            //Fazer conexão base de dados
+    };
+}
