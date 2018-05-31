5bbdbfe3cda957b1d25594426313aee6d75c0b81
ruipedromanta <39620242+ruipedromanta@users.noreply.github.com>
2018-05-31 21:11:26 +0100

    Connecting Activity´s

--- app/src/main/java/pt/ipg/quizzprogramao/MainActivity.java
+++ app/src/main/java/pt/ipg/quizzprogramao/MainActivity.java
@@ -1,31 +1,67 @@
 package pt.ipg.quizzprogramao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
 
 
+
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
-    Button resposta1, resposta2, resposta3, resposta4;
+    Button button9, button10, button11;
 
-    TextView score, pergunta;
 
-    private Perguntas mPerguntas = new Perguntas();
-
-    private String mResposta;
-    private int mScore;
-    private int mPerguntasLength = mPerguntas.mPerguntas.length;
-
-    Random r;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        button10 = findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openScore();
+
+
+            }
+        });
+
+        button9 = findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openQuizz();
+
+
+            }
+        });
     }
+
+
+    public void openScore() {
+        Intent intent = new Intent(this, Score.class);
+        startActivity(intent);
+    }
+
+
+    public void openQuizz() {
+        Intent intent = new Intent(this, Quizz.class);
+        startActivity(intent);
+    }
+
 }
+
+
+
+
+
