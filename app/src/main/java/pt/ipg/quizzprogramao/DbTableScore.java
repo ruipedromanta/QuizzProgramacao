36cfca4a4f36a8b6dad658f1f958cb0b30d0ce1e
ruipedromanta <39620242+ruipedromanta@users.noreply.github.com>
2018-06-01 12:28:42 +0100

    Update Score Table

--- app/src/main/java/pt/ipg/quizzprogramao/DbTableScore.java
+++ app/src/main/java/pt/ipg/quizzprogramao/DbTableScore.java
@@ -10,7 +10,6 @@ import android.provider.BaseColumns;
 public class DbTableScore implements BaseColumns {
 
     public static final String FIELD_SCORE = "score";
-    public static final String TABLE_NAME = FIELD_SCORE;
     public static final String FIELD_NAME = "name";
     private SQLiteDatabase db;
 
@@ -23,7 +22,7 @@ public class DbTableScore implements BaseColumns {
 
     public void create() {
         db.execSQL(
-                "CREATE TABLE " + TABLE_NAME + "(" +
+                "CREATE TABLE score(" +
                         _ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         FIELD_SCORE + "INTEGER," +
                         "FOREIGN KEY(" + FIELD_NAME + ") REFERENCES " +
