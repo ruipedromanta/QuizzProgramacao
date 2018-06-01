bee08edd43db517c10b7e9476ac07c73e7d589e2
ruipedromanta <39620242+ruipedromanta@users.noreply.github.com>
2018-06-01 12:25:35 +0100

    Created Score Table

---
+++ app/src/main/java/pt/ipg/quizzprogramao/DbTableScore.java
@@ -0,0 +1,39 @@
+package pt.ipg.quizzprogramao;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+/**
+ * Created by ruima on 01/06/2018.
+ */
+
+public class DbTableScore implements BaseColumns {
+
+    public static final String FIELD_SCORE = "score";
+    public static final String TABLE_NAME = FIELD_SCORE;
+    public static final String FIELD_NAME = "name";
+    private SQLiteDatabase db;
+
+    public DbTableScore(SQLiteDatabase db) {
+        this.db = db;
+
+    }
+
+
+
+    public void create() {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + "(" +
+                        _ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        FIELD_SCORE + "INTEGER," +
+                        "FOREIGN KEY(" + FIELD_NAME + ") REFERENCES " +
+                        DbTablePlayer.TABLE_NAME +
+                        "(" + DbTablePlayer._ID +")" +
+                        ")"
+
+
+
+
+        );
+    }
+}
