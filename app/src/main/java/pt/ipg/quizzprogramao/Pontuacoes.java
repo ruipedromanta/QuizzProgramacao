561052336f12947d09cb0b3818b0cc77b648b396
ruipedromanta <39620242+ruipedromanta@users.noreply.github.com>
2018-06-07 20:29:01 +0100

    Upgrade Layout´s and button´s

--- app/src/main/java/pt/ipg/quizzprogramao/Pontuacoes.java
+++ app/src/main/java/pt/ipg/quizzprogramao/Pontuacoes.java
@@ -39,5 +39,11 @@ public class Pontuacoes extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void clickSAIR(View v){
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
 }
 
