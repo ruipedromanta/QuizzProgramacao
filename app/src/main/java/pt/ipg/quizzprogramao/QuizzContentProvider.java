729f95339c1f29d02187a0cd413a97a1a22f1a4c
ruipedromanta <39620242+ruipedromanta@users.noreply.github.com>
2018-06-14 20:54:58 +0100

    Implement onCreate and query (quizz content provider)

--- app/src/main/java/pt/ipg/quizzprogramao/QuizzContentProvider.java
+++ app/src/main/java/pt/ipg/quizzprogramao/QuizzContentProvider.java
@@ -2,6 +2,7 @@ package pt.ipg.quizzprogramao;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -12,11 +13,39 @@ import android.support.annotation.Nullable;
  */
 
 public class QuizzContentProvider extends ContentProvider {
+    public static final int PLAYER = 100;
+    public static final String AUTHORITY = "pt.ipg.quizzprogramao";
+    public static final int PLAYER_ID = 101;
+    public static final int SCORE = 200;
+    public static final int SCORE_ID = 201;
+    public static final int QUESTIONS = 300;
+    public static final int QUESTIONS_ID = 301;
+    public static final int ANSWERS = 400;
+    public static final int ANSWERS_ID = 401;
+    DbQuizzOpenHelper quizzOpenHelper;
 
+
+
+        private static UriMatcher getQuizzUriMatcher() {
+
+            UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+            uriMatcher.addURI(AUTHORITY, "player", PLAYER);
+            uriMatcher.addURI(AUTHORITY, "player/#", PLAYER_ID);
+
+            uriMatcher.addURI(AUTHORITY, "score", SCORE);
+            uriMatcher.addURI(AUTHORITY, "score/#", SCORE_ID);
+
+            uriMatcher.addURI(AUTHORITY, "question", QUESTIONS);
+            uriMatcher.addURI(AUTHORITY, "questions/#", QUESTIONS_ID);
+
+            uriMatcher.addURI(AUTHORITY, "answers", ANSWERS);
+            uriMatcher.addURI(AUTHORITY, "answers/#", ANSWERS_ID);
+        }
 
     @Override
     public boolean onCreate() {
-        return false;
+        return true;
     }
 
     @Nullable
