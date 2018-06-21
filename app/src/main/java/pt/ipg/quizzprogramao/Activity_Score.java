f71abed4abd3792549a4999dc9cfd54903deff24
ruipedromanta <39620242+ruipedromanta@users.noreply.github.com>
2018-06-21 10:42:48 +0100

    Fixed Player Loader Manager

--- app/src/main/java/pt/ipg/quizzprogramao/Activity_Score.java
+++ app/src/main/java/pt/ipg/quizzprogramao/Activity_Score.java
@@ -1,38 +1,32 @@
 package pt.ipg.quizzprogramao;
 
-import android.app.LoaderManager;
-import android.content.CursorLoader;
+
+import android.content.Context;
 import android.content.Intent;
-import android.content.Loader;
 import android.database.Cursor;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CursorAdapter;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class Activity_Score extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     public static final int PLAYER_CURSOR_LOADER_ID = 0;
     private Button button;
-
-
-
-
-
-
     private RecyclerView recyclerViewPlayer;
     private PlayerCursorAdapter playerCursorAdapter;
 
-
-
     Button button7;
-    TextView textView, textView2, textView3, textView4, textView5, textView6;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +50,7 @@ public class Activity_Score extends AppCompatActivity implements LoaderManager.L
         playerCursorAdapter = new PlayerCursorAdapter(this);
         recyclerViewPlayer.setAdapter(playerCursorAdapter);
 
-        getLoaderManager().initLoader(PLAYER_CURSOR_LOADER_ID, null, this);
+        getLoaderManager().initLoader(PLAYER_CURSOR_LOADER_ID, null, (android.app.LoaderManager.LoaderCallbacks<Object>) this);
 
 
 
@@ -69,6 +63,11 @@ public class Activity_Score extends AppCompatActivity implements LoaderManager.L
         startActivity(intent);
     }
 
+    protected void onResume() {
+        super.onResume();
+        getSupportLoaderManager().restartLoader(PLAYER_CURSOR_LOADER_ID, null, this);
+    }
+
     public void clickSAIR(View v){
         moveTaskToBack(true);
         android.os.Process.killProcess(android.os.Process.myPid());
@@ -76,24 +75,25 @@ public class Activity_Score extends AppCompatActivity implements LoaderManager.L
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         if (id == PLAYER_CURSOR_LOADER_ID) {
-            return new CursorLoader(this, QuizzContentProvider.PLAYER_URI, DbTablePlayer.ALL_COLUMNS, null, null, null);
-
+            return new CursorLoader(this, QuizzContentProvider.PLAYER_URI, DbTablePlayer.ALL_COLUMNS, null, null,null);
         }
-
-
-
         return null;
     }
 
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 
-    }
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
+        @Override
+        public void onLoadFinished(@NonNull android.support.v4.content.Loader<Cursor> loader, Cursor data) {
+            playerCursorAdapter.refreshData(data);
+        }
+
+        @Override
+        public void onLoaderReset(@NonNull android.support.v4.content.Loader<Cursor> loader) {
+            playerCursorAdapter.refreshData(null);
+        }
+
 
     }
 }
