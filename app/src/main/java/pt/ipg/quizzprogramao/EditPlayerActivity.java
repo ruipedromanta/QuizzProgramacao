020efe3dc2eed08e029f484c08d55409525bd9b2
ruipedromanta <39620242+ruipedromanta@users.noreply.github.com>
2018-07-04 21:10:03 +0100

    Update some things in the java class and in the layouts

--- app/src/main/java/pt/ipg/quizzprogramao/EditPlayerActivity.java
+++ app/src/main/java/pt/ipg/quizzprogramao/EditPlayerActivity.java
@@ -1,5 +1,6 @@
 package pt.ipg.quizzprogramao;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -7,12 +8,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 /**
@@ -20,8 +25,12 @@ import android.widget.Toast;
  */
 
 public class EditPlayerActivity  extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
-        private EditText editTextName;
+    private static final int CATEGORIES_CURSOR_LOADER_ID = 0;
+
+    private EditText editTextName;
         private Player player;
+        private Spinner spinnerCategory;
+
 
 
 
@@ -30,6 +39,8 @@ public class EditPlayerActivity  extends AppCompatActivity implements LoaderMana
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_player);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
 
@@ -56,12 +67,21 @@ public class EditPlayerActivity  extends AppCompatActivity implements LoaderMana
         }
 
         editTextName = (EditText) findViewById(R.id.editTextName);
+        spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
 
 
         player = DbTablePlayer.getCurrentPlayerFromCursor(cursorPlayer);
+
         editTextName.setText(player.getName());
 
+        getSupportLoaderManager().restartLoader(CATEGORIES_CURSOR_LOADER_ID, null,this);
 
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getSupportLoaderManager().restartLoader(CATEGORIES_CURSOR_LOADER_ID, null, this);
     }
 
       public void Cancel(View view) {
@@ -69,8 +89,11 @@ public class EditPlayerActivity  extends AppCompatActivity implements LoaderMana
           }
 
     public void Save(View view) {
+        // todo: validations
 
         player.setName(editTextName.getText().toString());
+        player.setIdCategory((int) spinnerCategory.getSelectedItemId());
+
 
         int recordsAffected = getContentResolver().update(
                 Uri.withAppendedPath(QuizzContentProvider.PLAYER_URI, Integer.toString(player.getId())),
@@ -94,13 +117,91 @@ public class EditPlayerActivity  extends AppCompatActivity implements LoaderMana
     @NonNull
     @Override
     public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        if (id == CATEGORIES_CURSOR_LOADER_ID) {
+            return new CursorLoader(this, QuizzContentProvider.CATEGORIES_URI, DbTableCategories.ALL_COLLUMS, null, null, null);
+        }
+
         return null;
     }
+
+    /**
+     * Called when a previously created loader has finished its load.  Note
+     * that normally an application is <em>not</em> allowed to commit fragment
+     * transactions while in this call, since it can happen after an
+     * activity's state is saved.  See {@link FragmentManager#beginTransaction()
+     * FragmentManager.openTransaction()} for further discussion on this.
+     * <p>
+     * <p>This function is guaranteed to be called prior to the release of
+     * the last data that was supplied for this Loader.  At this point
+     * you should remove all use of the old data (since it will be released
+     * soon), but should not do your own release of the data since its Loader
+     * owns it and will take care of that.  The Loader will take care of
+     * management of its data so you don't have to.  In particular:
+     * <p>
+     * <ul>
+     * <li> <p>The Loader will monitor for changes to the data, and report
+     * them to you through new calls here.  You should not monitor the
+     * data yourself.  For example, if the data is a {@link Cursor}
+     * and you place it in a {@link CursorAdapter}, use
+     * the {@link CursorAdapter#CursorAdapter(Context, * Cursor, int)} constructor <em>without</em> passing
+     * in either {@link CursorAdapter#FLAG_AUTO_REQUERY}
+     * or {@link CursorAdapter#FLAG_REGISTER_CONTENT_OBSERVER}
+     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
+     * from doing its own observing of the Cursor, which is not needed since
+     * when a change happens you will get a new Cursor throw another call
+     * here.
+     * <li> The Loader will release the data once it knows the application
+     * is no longer using it.  For example, if the data is
+     * a {@link Cursor} from a {@link CursorLoader},
+     * you should not call close() on it yourself.  If the Cursor is being placed in a
+     * {@link CursorAdapter}, you should use the
+     * {@link CursorAdapter#swapCursor(Cursor)}
+     * method so that the old Cursor is not closed.
+     * </ul>
+     * <p>
+     * <p>This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that has finished.
+     * @param data   The data generated by the Loader.
+     */
 
     @Override
     public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
 
+        SimpleCursorAdapter cursorAdapterCategories = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                data,
+                new String[]{DbTableCategories.FIELD_NAME},
+                new int[]{android.R.id.text1}
+        );
+
+        spinnerCategory.setAdapter(cursorAdapterCategories);
+
+        int idCategory = player.getIdCategory();
+
+        for (int i = 0; i < spinnerCategory.getCount(); i++) {
+            Cursor cursor = (Cursor) spinnerCategory.getItemAtPosition(i);
+
+            final int posId = cursor.getColumnIndex(DbTableCategories._ID);
+
+            if (idCategory == cursor.getInt(posId)) {
+                spinnerCategory.setSelection(i);
+                break;
+            }
+        }
     }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.  The application should at this point
+     * remove any references it has to the Loader's data.
+     * <p>
+     * <p>This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that is being reset.
+     */
+
 
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) {
